e7871b5cc5ff45e299d3971f0da748b0849ca15d
FrankoBelbino <91891068+FrankoBelbino@users.noreply.github.com>
2022-02-23 15:33:21 +0000

    Stamina regen for parrot has been fixed

--- IP2-Group-7/Assets/Scripts/birdMovement.cs
+++ IP2-Group-7/Assets/Scripts/birdMovement.cs
@@ -27,7 +27,7 @@ public class birdMovement : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         staminaBar.SetMaxStamina(maxStamina);
-        StartCoroutine("staminaRegen");
+        StartCoroutine("StaminaRegen");
     }
 
     // Update is called once per frame
@@ -40,46 +40,47 @@ public class birdMovement : MonoBehaviour
             velocity.y = -2f;            
         }        
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && currentStamina > 0)
         {
             velocity.y = 5f;
             takeStamina(1);
         }       
-
-        void takeStamina(int staminaLost)
-        {
-            currentStamina -= staminaLost;
-
-            staminaBar.SetStamina(currentStamina);
-        }
-
-        void gainStamina(int staminaGain)
-        {
-            currentStamina += staminaGain;
-
-            staminaBar.SetStamina(currentStamina);
-        }
-
-        IEnumerator staminaRegen()
-        {
-            for (; ; )
-            {
-                if (isGrounded == true)
-                {
-                    yield return new WaitForSeconds(0.5f);
-                    gainStamina(1);
-                }
-            }                                               
-        }
-
+       
+        
+       
         float x = Input.GetAxis("Horizontal");
 
         Vector3 move = transform.right * x;
         controller.Move(move * speed * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);       
+    }
 
-        
+    IEnumerator StaminaRegen()
+    {
+        for (; ; )
+        {
+            
+            if (isGrounded == true)
+            {
+                yield return new WaitForSeconds(0.5f);
+                gainStamina(1);
+            }
+        }
+    }
+
+    void takeStamina(int staminaLost)
+    {
+        currentStamina -= staminaLost;
+
+        staminaBar.SetStamina(currentStamina);
+    }
+
+    void gainStamina(int staminaGain)
+    {
+        currentStamina += staminaGain;
+
+        staminaBar.SetStamina(currentStamina);
     }
 }
