acc2d313e03e98edb26b95af716cc505115dbbfe
FrankoBelbino <91891068+FrankoBelbino@users.noreply.github.com>
2022-03-25 13:00:26 +0000

    Multiple Changes
    Blocked out some of the play area and added the framework for the pause menu

---
+++ IP2-Group-7/Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    public GameObject menu;
+    public string pauseMenuActive;
+
+    void Start()
+    {
+        menu.SetActive(false);
+        Time.timeScale = 1f;
+        pauseMenuActive = "false";
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && pauseMenuActive == "false")
+        {
+            Pause();
+        }
+        if (Input.GetKeyDown(KeyCode.P) && pauseMenuActive == "true")
+        {
+            Resume();
+        }
+    }
+
+    public void Resume()
+    {
+        menu.SetActive(false);
+        Time.timeScale = 1f;
+        pauseMenuActive = "false";
+    }
+
+    public void Pause()
+    {
+        menu.SetActive(true);
+        Time.timeScale = 0f;
+        pauseMenuActive = "true";
+    }
+}
